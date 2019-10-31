fa7ec272fd99ac9fca89970454001a437c29a5af
tak kuriyama <phantom.world.hokage@gmail.com>
2019-10-31 22:14:09 +0900

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,10 @@ eval (python3 -m virtualfish compat_aliases)
 set -U DOT_REPO https://github.com/tak-kuriyama/dotfiles.git
 set -U DOT_DIR ~/.dotfiles
 set -U DOT_SCR ~/.dot
+
+set PATH $HOME/.cargo/bin $PATH
+
+alias gs 'git status'
+alias gc 'git commit'
+alias ga 'git add'
+alias ls 'exa'
